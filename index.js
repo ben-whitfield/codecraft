0a4dd0716b68dd4cfa3caeefa871cb3aed288eda
Ben Whitfield <ben.whitfield@bbc.co.uk>
2020-09-18 19:03:39 +0100

    output in correct formatting

--- index.js
+++ index.js
@@ -7,17 +7,10 @@ const screenOutput = function (userInput) {
     const lineArray = charArray(userInput)
     for (let index = 0; index < 3; index++) {
         console.log(constructLine(lineArray, index))
-    }
-
-//     const output =  `
-//     __
-//    |__|
-//    |__|
-//    `
-//     return output   
+    }  
 };
 
 console.log('The User Input was:', userInput)
 console.log('The output is:')
-console.log(screenOutput(userInput))
-
+screenOutput(userInput)
+console.log(``)

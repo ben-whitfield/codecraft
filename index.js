f0677e877c4ae040d71feb9da20c476982b7b19a
Ben Whitfield <ben.whitfield@bbc.co.uk>
2020-09-29 21:26:12 +0100

    basic animation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const constructLine = require('./constructLine');
 const charArray = require('./constructArray');
+const customOut = require('./utilities');
 
 const userNumber = process.argv[2];
 const userSize = process.argv[3];
+const screenWidth = 120
 
 const screenOutput = function (userNumber, userSize) {
     if(userSize < 2) userSize = 2
@@ -10,16 +12,37 @@ const screenOutput = function (userNumber, userSize) {
     const totalLines = (userSize * 2)
     const midLine = parseInt(userSize)
 
-    for (let currLine = 0; currLine < totalLines+1; currLine++) {
-        if (currLine === 0) { console.log(constructLine(lineArray, 'top', userSize)) } 
-        else if (currLine === totalLines) { console.log(constructLine(lineArray, 'bottom', userSize)) }
-        else if (currLine === midLine) { console.log(constructLine(lineArray, 'middle', userSize)) }
-        else if (currLine > midLine) { console.log(constructLine(lineArray, 'lowerFiller', userSize)) }
-        else if (currLine < midLine) { console.log(constructLine(lineArray, 'upperFiller', userSize)) }
-    }  
+    customOut.clearScreen
+    console.clear();
+    console.log('starting')
+    
+    let index = screenWidth;
+
+    const myInterval = setInterval(() => {
+        index-- 
+            let completeOutput;
+            for (let currLine = 0; currLine < totalLines+1; currLine++) {
+                let output = ``
+                if (currLine === 0) { output = constructLine(lineArray, 'top', userSize) } 
+                else if (currLine === totalLines) { output = constructLine(lineArray, 'bottom', userSize) }
+                else if (currLine === midLine) { output = constructLine(lineArray, 'middle', userSize) }
+                else if (currLine > midLine) { output = constructLine(lineArray, 'lowerFiller', userSize) }
+                else if (currLine < midLine) { output = constructLine(lineArray, 'upperFiller', userSize) }
+                spaces = `${' '.repeat(parseInt(index))}`
+                lineOutput = `${spaces+output}`
+                completeOutput = `${completeOutput}\n${lineOutput}`
+            }
+            process.stdout.write('\033[H')
+            process.stdout.write(`${completeOutput}`)
+            customOut.hide;
+            
+        if(index === 0) {
+            clearInterval(myInterval)
+        }
+    }, 100);
 };
 
-console.log('The User Input was:', userNumber, 'and the size was: ', userSize)
-console.log('The output is:')
+// console.log('The User Input was:', userNumber, 'and the size was: ', userSize)
+// console.log('The output is:')
 screenOutput(userNumber, userSize)
-console.log(``)
+// console.log(``)

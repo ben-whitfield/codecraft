688c4f391b4f3e35dd4d7304e80beb08a0f10bd3
Ben Whitfield <ben.whitfield@bbc.co.uk>
2020-10-01 09:18:59 +0100

    add number segments builder

--- Task4/buildNumber.js
+++ Task4/buildNumber.js
@@ -1,9 +1,25 @@
 const segments = require('./segments');
 
+const charValues = { 'A': "_", 'B': "|", 'C': "|", 'D': "_", "E": '|', "F": '|', "G": '_' }
 
-const buildNumber = () => {
-
+const builtSections = (num) => {
+    const numberSections = { 'A': ``, 'B': ``, 'C': ``, 'D': ``, 'E': ``, 'F': ``, 'G': `` }
+    const numberStructure = segments(num)
+    for ( const item in numberStructure ) {
+        numberStructure[item] === 1 ? numberSections[item] = charValues[item] : numberSections[item] = ''
+    }
+    return numberSections
 }
 
+const buildNumber = (num, scale) => {
+    // construct the string output
+    let builtNumber = ``
+    
+    builtNumber `${' '.repeat(parseInt(size))}`
+    
+    
+    return builtNumber
+}
 
-module.exports = buildNumber
+console.log(buildNumber(4,1))
+// module.exports = buildNumber

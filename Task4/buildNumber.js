52715937a9bebd1e8e05e19a231e0aa036091a45
Ben Whitfield <ben.whitfield@bbc.co.uk>
2020-10-01 11:03:04 +0100

    add segments repeats

--- Task4/buildNumber.js
+++ Task4/buildNumber.js
@@ -3,16 +3,15 @@ const segments = require('./segments');
 function buildTop(char, size) {
     return ` ${char.repeat(parseInt(size))} `
 }
-
 function buildLeft(char, size) {
-    return ` ${char,' '.repeat(parseInt(size-1))} `
+    return ` ${char}${' '.repeat(parseInt(size-1))} `
 }
 function buildRight(char, size) {
-    return ` ${' '.repeat(parseInt(size-1),char)} `
+    return ` ${' '.repeat(parseInt(size-1))}${char} `
 }
-
 function buildMid(left, middle, right, size) {
-    return ` ${left,middle.repeat(parseInt(size-2)), right}`
+    const repeatSize = (size-2 < 3) ? 2 : size-2
+    return ` ${left}${middle.repeat(parseInt(repeatSize))}${right}`
 }
 
 const charValues = { 'A': "_", 'B': "|", 'C': "|", 'D': "_", "E": '|', "F": '|', "G": '_' }
@@ -27,13 +26,22 @@ const builtSections = (num) => {
 }
 
 const buildNumber = (num, scale) => {
-    let builtNumber = ``
+    const sections = builtSections(num)
+    const totalLines = (scale * 2)
+    const midLine = parseInt(scale)
+    let builtNumber = {
+        'A': `${buildTop(sections['A'],scale)}`,
+        'B': `${buildRight(sections['B'],scale)}`,
+        'C': `${buildRight(sections['C'],scale)}`,
+        'D': `${buildMid(sections['E'],sections['D'],sections['C'],scale)}`,
+        'E': `${buildLeft(sections['E'],scale)}`,
+        'F': `${buildLeft(sections['F'],scale)}`,
+        'G': `${buildMid(sections['F'],sections['G'],sections['E'],scale)}`,
     
-    builtNumber `${' '.repeat(parseInt(size))}`
-    
-    
+    }
+
     return builtNumber
 }
 
-console.log(buildNumber(4,1))
+console.log(buildNumber(4,3))
 // module.exports = buildNumber

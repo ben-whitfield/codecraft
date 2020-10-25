b9e378fb55cdb27dd718bf6ea547e7b4a0766d6b
Ben Whitfield <ben.whitfield@bbc.co.uk>
2020-10-25 16:04:53 +0000

    scrolling clock working

--- buildDisplay.js
+++ buildDisplay.js
@@ -3,8 +3,9 @@ const displayWidth = 120
 const buildDisplay = (endTime, startPos) => {
     const stringLines = endTime.split('\n')
     const newString = stringLines
+    const display = displayWidth + startPos
     
-    while (newString[0].length < displayWidth ) {
+    while (newString[0].length < display) {
         for (let index = 0; index < stringLines.length; index++) {
             newString[index] = `${newString[index]} ${newString[index]}`
         }
@@ -12,7 +13,7 @@ const buildDisplay = (endTime, startPos) => {
 
     let displayString = ``
     newString.forEach(element => {
-        displayString += `${element.slice(0, displayWidth)}\n`
+        displayString += `${element.slice(startPos, display)}\n`
     });
    
     return displayString
